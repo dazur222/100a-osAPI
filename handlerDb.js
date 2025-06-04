c98b50d2fb8bcf58b3eb916cf78d2480e7d4a093
Yado3312 <goyado4@gmail.com>
2025-06-04 09:28:03 -0600

    FUNCIONA

---
+++ handlerDb.js
@@ -0,0 +1,21 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+    await client.close();
+  }
+}
+run().catch(console.dir);
